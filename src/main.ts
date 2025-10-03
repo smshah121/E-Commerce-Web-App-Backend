fd4dfa279ea5c29ff2050e10fb932f5481865b23
smshah121 <smshah.2003@gmail.com>
2025-10-03 19:41:06 +0500

    changes saved

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,26 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true,  transformOptions: {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
         enableImplicitConversion: true, // converts string to number, boolean, etc.
-      },}))
+      },
+    }),
+  );
   app.enableCors({
-     origin: [
-    "https://dynamic-griffin-d0c7fd.netlify.app", // your frontend domain
-  ],
-  })
-  
-  
+    origin: ['https://dynamic-griffin-d0c7fd.netlify.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // allows cookies/auth headers
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0'); // crucial for cloud deployment
   console.log(`Backend running on port ${port}`);
-  
 }
 bootstrap();
