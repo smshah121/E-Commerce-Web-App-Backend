c385bfab5da680159f69eb87900478d928f4636f
smshah121 <smshah.2003@gmail.com>
2026-04-12 23:14:20 +0500

    cs

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { OrderModule } from 'src/order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from 'src/order/entities/order.entity';
+import { Product } from 'src/product/entities/product.entity';
+import { OrderItem } from 'src/order-item/entities/order-item.entity';
+import { ProductImage } from 'src/product-image/entities/product-image.entity';
 
 @Module({
-  imports: [OrderModule],
+  imports: [TypeOrmModule.forFeature([
+    Order,
+    Product,
+    OrderItem,
+    ProductImage
+  ]),OrderModule],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
